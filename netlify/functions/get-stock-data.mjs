28faddee5556774a5d59f067b2ef49c157e57a17
Csaba Boros <boros.csaba94@gmail.com>
2022-03-28 20:07:08 +0300

    Fetch from netlify function

--- netlify/functions/get-stock-data.mjs
+++ netlify/functions/get-stock-data.mjs
@@ -1,8 +1,12 @@
+import fetch from 'node-fetch';
+
 exports.handler = async (event) => {
     try {
+        const response = await fetch('https://yfapi.net/v8/finance/spark?interval=1d&range=max&symbols=SXR8.DE%2CIUSN.DE%2CSXRV.DE');
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ value: "OK" })
+            body: JSON.stringify(response)
         };
     }
     catch (error) {
