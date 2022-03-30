e29884a7c08a1cc374eeff26198c0fbc10a5eba5
Csaba Boros <boros.csaba94@gmail.com>
2022-03-30 11:51:47 +0300

    Loop through portfolio items

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <Portfolio />
+      <Portfolio portfolioItems={portfolioItems} />
     </div>
   );
 }
--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const Portfolio = () => {
+const Portfolio = ({portfolioItems}) => {
     return (
         <div>
             <h1>Portfolio component</h1>
@@ -10,9 +10,11 @@ const Portfolio = () => {
                 <Row>
                     <Col><b>Ticker</b></Col>
                 </Row>
-                <Row>
-                    <Col>Test</Col>
-                </Row>
+                {portfolioItems.map(item =>
+                    <Row>
+                        <Col>{item.ticker}</Col>
+                    </Row>
+                )}
             </Container>
         </div>
     );
